0ad56b52bd6914de20f90e136f9dcb2f3df870e2
Snigdha Susana Chandra Pathroju <sscpathroju@adaequare.com>
2022-02-18 16:35:56 +0530

    commit

---
+++ src/main/java/test/testdata.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testdata {
+	@Test
+	
+	public void gmail() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("https://github.com/",Keys.ENTER);
+	}
+
+}
